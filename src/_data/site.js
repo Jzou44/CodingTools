dd8244bc33a7bd55278de0b5f123accec0cfb496
bylearner <bylearner@gmail.com>
2026-05-12 22:53:08 +0800

    Bump asset version for tool CSS

--- src/_data/site.js
+++ src/_data/site.js
@@ -37,7 +37,7 @@ function absoluteUrl(path) {
 module.exports = {
   baseUrl,
   siteName: "Coding.Tools",
-  assetVersion: "20260508-1",
+  assetVersion: "20260512-1",
   ogImage: "/assets/favicons/favicon-512x512.png",
   languages,
   languageIds: languages.map((item) => item.id),
